50ab2c656f8d102d3c701ee4dba9201a7f5f7a0c
Elton M. C. <eltonm.cardoso@gmail.com>
2024-05-10 17:27:22 -0300

    TestFail

--- test/cover-test-50k.rkt
+++
@@ -1,8 +0,0 @@
-#lang racket
-(require "solver-test.rkt"
-         rackcheck)
-
-(check-property  (make-config #:tests 50000
-                              #:deadline (* (+ (current-inexact-milliseconds) 3600000) 4))
-                 accept-well-typed)
-
